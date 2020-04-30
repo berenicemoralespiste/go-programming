94426164d7d70b73aabf52cc75970f85f1336198
berenicemoralespiste <your_email@example.com>
2020-04-29 19:56:16 -0500

    04 mutex ya no tiene condition race al bloquear la goroutine

---
+++ 00-testing/04-mutex/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+"fmt"
+"runtime"
+"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	incremento := 0
+	gs := 100
+	wg.Add(gs)
+	var mt sync.Mutex
+
+	for i := 0; i < gs ; i ++ {
+		go func() {
+			mt.Lock()
+			v := incremento
+			runtime.Gosched()
+			v++
+			fmt.Println(incremento)
+			incremento = v
+			mt.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("El valor de la variable incremento es : ", incremento)
+}
